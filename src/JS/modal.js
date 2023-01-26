06cca94e34efebfe505b99e8e59682d853e51d95
Sayonara <alekss3228@gmail.com>
2023-01-26 14:46:48 +0200

    class-modal

---
+++ src/JS/modal.js
@@ -0,0 +1,21 @@
+
+
+const buttonIp = document.querySelector("#login-btn")
+
+buttonIp.addEventListener("click",  () => {new Modal().user()})
+
+class Modal {
+
+    user(){
+        const element = document.createElement('div')
+        element.classList = "user"
+        element.innerHTML = `
+                <p>Login</p>
+                <p id = "gogi">Password</p>
+                `
+        element.style.cssText = `background-color: #f2f2f2; border: 1px solid black; padding: 40px; text-align: center; margin: 10px 300px;`
+        document.body.append(element)
+
+    }
+
+}
