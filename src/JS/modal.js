a7fedcf047ece5bd0a9e4121509d91a0c8881885
Никита Голуб <sleipnir1148@gmail.com>
2023-01-27 16:52:39 +0200

    create modal window in js file

--- src/JS/modal.js
+++ src/JS/modal.js
@@ -1,20 +1,45 @@
+const buttonIp = document.querySelector('#login-btn');
 
-
-const buttonIp = document.querySelector("#login-btn")
-
-buttonIp.addEventListener("click",  () => {new Modal().user()})
+buttonIp.addEventListener('click',  () => { new Modal().user() })
 
 class Modal {
-
-    user(){
+    user() {
         const element = document.createElement('div')
-        element.classList = "user"
+        const elementBlur = document.createElement('div')
+
+        element.id = 'idUser';
+        elementBlur.id = 'idBlur';
         element.innerHTML = `
-                <p>Login</p>
-                <p id = "gogi">Password</p>
-                `
-        element.style.cssText = `background-color: #f2f2f2; border: 1px solid black; padding: 40px; text-align: center; margin: 10px 300px;`
-        document.body.append(element)
+                <div class="login-popup text-center container-md">
+                    <label for="basic-url" class="form-label text-uppercase text-dark"><h2>Вхід</h2></label>
+                    <div class="input-group mb-2">
+                    <span class="input-group-text" id="basic-addon-one">@</span>
+                    <input type="text" class="form-control" id="basic-url-one" aria-describedby="basic-addon3"
+                    placeholder="example@mail.com">
+                    </div>
+                    <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon-two">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-key" viewBox="0 0 16 16">
+                    <path
+                    d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8zm4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5z"/>
+                    <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                    </svg>
+                    </span>
+                    <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3"
+                     placeholder="enter your password">
+                    </div>
+                    <div class="buttons-block d-flex justify-content-center align-items-center">
+                    <button type="button" class="btn btn-secondary me-2" style="width: 100px">Закрити</button>
+                    <button type="button" class="btn btn-danger" style="width: 100px">Увійти</button>
+                    </div>
+                </div>
+                  `
+        element.style.cssText = 'max-width: 400px; margin: 0 auto; position: relative; top: 50%; transform: translateY(-50%); background-color: white; padding: 30px; border-radius: 15px;';
+        elementBlur.style.cssText = 'background-color: rgba(0, 0, 0, .3); position: fixed; top: 0; left: 0; right: 0; bottom: 0; z-index: 999; backdrop-filter: blur(1px);'
+        elementBlur.append(element)
+        document.body.append(elementBlur)
+
     }
 
 }
