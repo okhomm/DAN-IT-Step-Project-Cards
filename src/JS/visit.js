87a029adabac8064555c945d85122081c4e70c91
Sayonara <alekss3228@gmail.com>
2023-01-27 16:49:52 +0200

    class-visit

---
+++ src/JS/visit.js
@@ -0,0 +1,17 @@
+
+
+class Visit {
+
+
+
+}
+
+// class VisitDentist extends Visit {
+//
+// }
+// class VisitCardiologist extends Visit {
+//
+// }
+// class VisitTherapist extends Visit {
+//
+// }
